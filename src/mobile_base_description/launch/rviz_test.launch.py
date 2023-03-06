81e6c4caa01479dc1b9ec95bddd9724b86945d67
lass6230 <due6230@gmail.com>
2023-03-06 17:20:54 +0100

    zxc

--- src/mobile_base_description/launch/rviz_test.launch.py
+++ src/mobile_base_description/launch/rviz_test.launch.py
@@ -86,7 +86,7 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='False',
                                             description='Flag to enable use_sim_time'),
         #joint_state_publisher_node,
-        joint_state_publisher_gui_node,
+        #joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node,
         robot_localization_node
