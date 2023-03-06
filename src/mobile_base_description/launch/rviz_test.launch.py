9d5a2ff3352182dee4dd438f743579d94b63c034
lass6230 <due6230@gmail.com>
2023-03-06 15:52:51 +0100

    asd

--- src/mobile_base_description/launch/rviz_test.launch.py
+++ src/mobile_base_description/launch/rviz_test.launch.py
@@ -48,12 +48,12 @@ def generate_launch_description():
         parameters=[robot_description],
         #parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-    )
+   # joint_state_publisher_node = launch_ros.actions.Node(
+   #     package='joint_state_publisher',
+   #     executable='joint_state_publisher',
+   #     name='joint_state_publisher',
+   #     condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+   # )
     joint_state_publisher_gui_node = launch_ros.actions.Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
@@ -85,7 +85,7 @@ def generate_launch_description():
                                             description='Absolute path to rviz config file'),
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='False',
                                             description='Flag to enable use_sim_time'),
-        joint_state_publisher_node,
+        #joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node,
