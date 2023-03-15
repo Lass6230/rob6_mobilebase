a299cbd4874cc35bc497865a846d9fca7c6ce62c
simsimbappel <simontk2010@hotmail.com>
2023-03-15 11:23:01 +0100

    rs launch with updated parameters

--- src/opencv_detector/setup.py
+++ src/opencv_detector/setup.py
@@ -1,15 +1,18 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'opencv_detector'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,3 +27,4 @@ setup(
         ],
     },
 )
+
